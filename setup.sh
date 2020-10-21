1a2206d372a91e1a32f1e7645156bd05b02d22c0
Marco Antonio <masjr@amazon.com>
2020-10-21 13:40:07 -0300

    FEA: conditional copy

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 if [[ ! -e $HOME/.SpaceVim.d/ ]]; then
-  echo Installing SpaceVim
+  echo Installing SpaceVim...
   curl -sLf https://spacevim.org/install.sh | bash
 fi
 
-echo Copying SpaceVim configs
-curl -s https://raw.githubusercontent.com/marco-souza/vim-setup/main/init.toml > $HOME/.SpaceVim.d/init.toml
+echo Copying SpaceVim configs...
+if [[ -e ./init.toml ]]; then
+  cat ./init.toml > $HOME/.SpaceVim.d/init.toml
+else
+  curl https://raw.githubusercontent.com/marco-souza/vim-setup/main/init.toml > $HOME/.SpaceVim.d/init.toml
+fi
 
 echo "Your vim is ready to go :)"
